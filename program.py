299bffc64a0ad6ae301a549c2d65e9188d26b347
ar7u9o <kevin.k1252@gmail.com>
2013-06-14 16:42:49 +0200

    added origin

--- program.py
+++ program.py
@@ -5,7 +5,7 @@ import os
 import sys
 import pipes
 
-f_name = sys.argv[1]
+f_name = 'getMatch{}'.format(sys.argv[1][0].upper() + sys.argv[1][1:])
 
 r = requests.get('http://101companies.org/endpoint/Language:Python/json/directions').text
 
@@ -91,13 +91,32 @@ for c in data:
             mindiffratio = getMatchCluster(clusters[cluster]['files'] + [cluster], cs[c][f])
             if mindiffratio >= 0.25:
                 clusters[cluster]['files'].append(f)
-                if mindiffratio < clusters[cluster].get('mindiffratio', 1):
-                    clusters[cluster]['mindiffratio'] = mindiffratio
+                #if mindiffratio < clusters[cluster].get('mindiffratio', 1):
+                #    clusters[cluster]['mindiffratio'] = mindiffratio
 
 
 for c in clusters.keys():
     if len(clusters[c]['files']) == 1:
         del clusters[c]
 
+for c in clusters.keys():
+    origin = None
+    for f in clusters[c]['files']:
+        url = 'http://101companies.org/resources/contributions/{}'.format(f)
+        github = json.loads(requests.get(url).text)['github']
+        github = github.split('/')
+        #github = github.replace('http://', 'https://')
+        #github = github.replace('/tree/master', '')        
+        #github = github.replace('github.com', 'api.github.com/repos')
+        url = 'https://api.github.com/repos/{user}/{repo}/commits?path={path}'.format(user=github[3], repo=github[4], path='/'.join(github[7:]))
+        info = json.loads(requests.get(url).text)
+        info = map(lambda c: c['commit']['author'], info)
+        first = sorted(info, key=lambda i: i['date'])[0]
+        if not origin or origin['commit']['date'] > first['date']:
+            origin = {
+                'file': f,
+                'commit': first
+            }
+    clusters[c]['origin'] = origin
 
 print json.dumps(clusters)
